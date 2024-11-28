169e8e7fab29da8a5a95b119fa671e426b6261cc
boffart <>
2024-11-28 10:25:09 +0300

    #840

--- src/Core/Workers/Libs/WorkerModelsEvents/Actions/ReloadVoicemailAction.php
+++ src/Core/Workers/Libs/WorkerModelsEvents/Actions/ReloadVoicemailAction.php
@@ -3,6 +3,8 @@
 namespace MikoPBX\Core\Workers\Libs\WorkerModelsEvents\Actions;
 
 use MikoPBX\Core\System\PBX;
+use MikoPBX\Core\System\Util;
+use MikoPBX\Core\Workers\Cron\WorkerSafeScriptsCore;
 
 class ReloadVoicemailAction implements ReloadActionInterface
 {
@@ -15,5 +17,10 @@ class ReloadVoicemailAction implements ReloadActionInterface
     public function execute(array $parameters = []): void
     {
         PBX::voicemailReload();
+        // Restart WorkerNotifyByEmail;
+        $pbxConsole = Util::which('pbx-console');
+        shell_exec("$pbxConsole service WorkerNotifyByEmail stop");
+        $worker = new WorkerSafeScriptsCore();
+        $worker->start(['start']);
     }
 }
