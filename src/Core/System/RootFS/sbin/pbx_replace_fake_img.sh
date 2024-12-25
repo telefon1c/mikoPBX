af81ffcc7f7cbc11ec5bd1e1cdf922016a4dee10
boffart <>
2024-12-25 14:19:44 +0300

    #837 Правка ошибок в работе скрипта.

--- src/Core/System/RootFS/sbin/pbx_replace_fake_img.sh
+++ src/Core/System/RootFS/sbin/pbx_replace_fake_img.sh
@@ -24,46 +24,46 @@ fi
 # Задание переменных
 MOUNT_DIR="/mnt"
 STORAGE_DIR="/storage/usbdisk1"
-FIRMWARE_IMG_PATH="/mnt/firmware.img"
+FIRMWARE_IMG_PATH="$MOUNT_DIR/firmware.img"
 RAW_IMG_FILE="${UPDATE_IMG_FILE}_D.raw"
 
 # Создание необходимых директорий
-mkdir -p "$MOUNT_DIR" "$STORAGE_DIR"
+mkdir -p "$MOUNT_DIR" "$STORAGE_DIR";
 
-echo " - mount storage for upgrade"
-mount -rw UUID="$STORAGE_UUID" "$STORAGE_DIR" 2> /dev/null
+echo " - mount storage for upgrade";
+mount -rw UUID="$STORAGE_UUID" "$STORAGE_DIR" 2> /dev/null;
 MOUNT_RESULT=$?
 if [ "$MOUNT_RESULT" -ne 0 ]; then
   echo " - Fail mount storage with UUID=$STORAGE_UUID to $STORAGE_DIR ..."
   exit 1
 fi
 
-echo " - unpack image file to raw format"
-/bin/busybox gunzip -c "$UPDATE_IMG_FILE" > "$RAW_IMG_FILE"
-loopName="$(losetup --show -f -o 524288 "$RAW_IMG_FILE")"
+echo " - unpack image file to raw format";
+/bin/busybox gunzip -c "$UPDATE_IMG_FILE" > "$RAW_IMG_FILE";
+echo " - gunzip return $?";
+loopName="$(losetup --show -f -o 524288 "$RAW_IMG_FILE")";
 
-echo " - mount raw image file"
-mount -t vfat "$loopName" "$MOUNT_DIR" -o ro,umask=0000
-MOUNT_RESULT=$?
+echo " - mount raw image file";
+mount -t vfat "$loopName" "$MOUNT_DIR" -o ro,umask=0000;
+MOUNT_RESULT=$?;
 if [ "$MOUNT_RESULT" -ne 0 ]; then
-  echo " - Fail mount img with $loopName to $MOUNT_DIR ..."
-  rm "$RAW_IMG_FILE"
+  echo " - Fail mount img with $loopName to $MOUNT_DIR ...";
+  rm "$RAW_IMG_FILE";
   exit 1
 fi
 
 if [ -f "$FIRMWARE_IMG_PATH" ]; then
-  echo " - replace fake image file";
-  /bin/busybox md5sum "$FIRMWARE_IMG_PATH" "$UPDATE_IMG_FILE";
   cp "$FIRMWARE_IMG_PATH" "$UPDATE_IMG_FILE";
+  echo " - replace fake image file result $?";
 else
   echo " - FAIL replace fake image file";
 fi
 
 echo " - unmount and clean temp files...";
-umount "$MOUNT_DIR"
-losetup -d "$loopName"
-rm "$RAW_IMG_FILE"
-umount "$STORAGE_DIR"
+umount "$MOUNT_DIR";
+losetup -d "$loopName";
+rm "$RAW_IMG_FILE";
+umount "$STORAGE_DIR";
 
 echo " - unmount offload and storage partition, and clean temp files...";
 freeupoffload;
